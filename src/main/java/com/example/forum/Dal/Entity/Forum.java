45d6b68f44301257c0ac67353fe7a960817a98cc
Tom-progg <tom.wat@hotmail.com>
2020-02-12 11:43:02 +0100

    hello

--- src/main/java/com/example/forum/Dal/Entity/Forum.java
+++ src/main/java/com/example/forum/Dal/Entity/Forum.java
@@ -1,4 +1,29 @@
 package com.example.forum.Dal.Entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Forum {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Forum_Id")
+    private Long id;
+
+    @Column(name = "Date_de_creation")
+    private Date dateDeCreation;
+
+    @Column(name ="Forum_name")
+    private String nom;
+
+    
+
 }
