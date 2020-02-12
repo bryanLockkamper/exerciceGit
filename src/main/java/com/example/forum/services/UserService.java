ce60a27759e6b5c7715374033e86ebcc6d63703d
=bastienhousiaux <bastienhousiaux@gmail.com>
2020-02-12 11:35:25 +0100

    UserService definition

--- src/main/java/com/example/forum/services/UserService.java
+++ src/main/java/com/example/forum/services/UserService.java
@@ -1,4 +1,14 @@
 package com.example.forum.services;
 
-public interface UserService {
+import org.springframework.data.repository.CrudRepository;
+
+// TODO: replace T with UserDTO when updated
+public interface UserService<T,Long> extends CrudRepository<T,Long> {
+    void loginUser();
+    void logoutUser();
+    Object getUserPostedMessage();
+    long getUserMessageCount();
+    void deleteAllMessages();
+    void deleteAllMessagesOnForum(Object forum);
 }
+
