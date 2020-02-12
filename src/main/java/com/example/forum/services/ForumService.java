bfbcdf393ad1878f71c2daaf90222ec29d715968
Comamanel <florent.piette9@gmail.com>
2020-02-12 11:48:09 +0100

    finished forumservice

--- src/main/java/com/example/forum/services/ForumService.java
+++ src/main/java/com/example/forum/services/ForumService.java
@@ -2,8 +2,11 @@ package com.example.forum.services;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ForumService<T> extends CrudRepository<T, Long> {
-    Optional<T> findByUser(T user);
+    List<T> findByCreator(Object user);
+    List<T> findByCreationDate(String date);
+    Optional<T> findByName(String name);
 }
